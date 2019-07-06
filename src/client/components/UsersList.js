ca38dfbaad8213bdb94446ddc3da85f6dfe02c8c
Vijay Ranghar <vijayranghar@gmail.com>
2019-07-07 00:58:54 +0530

    setup redu for client and server side

---
+++ src/client/components/UsersList.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { fetchUsers } from '../actions'
+
+class UserList extends Component { 
+  componentDidMount() {
+    this.props.fetchUsers()
+  }
+  renderUsers() {
+    return this.props.users.map((user, index) => {
+      return <li key={index}>{user.name}</li>
+    })
+  }
+  render() {
+    return (
+      <div>
+        A list of users
+        <ul>
+          {this.renderUsers()}
+        </ul>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = (state) => ({
+  users: state.users
+})
+
+export default connect(mapStateToProps, { fetchUsers })(UserList)
--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
+import 'babel-polyfill'
 import express from 'express'
-import React from 'react'
 import renderer from './helper/renderer'
-
+import createStore from './helper/createStore'
 const app = express()
 
 app.use(express.static('public'))
 
-app.get('/',(req, res) => {
-  res.send(renderer(req))
+app.get('*',(req, res) => {
+  const store = createStore()
+  //some logic to initialize 
+  // and load data into the store
+  res.send(renderer(req, store))
 })
 
 app.listen(8888,() => {
