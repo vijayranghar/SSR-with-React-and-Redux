fd7dcefb6979948b8206e9c0eea8b0d89a292261
Vijay Ranghar <vijayranghar@gmail.com>
2019-07-21 16:53:12 +0530

    serialize redux data to prevent XSS attack

--- build/bundle.js
+++ build/bundle.js
@@ -396,6 +396,10 @@ var _reactRedux = __webpack_require__(3);
 
 var _reactRouterConfig = __webpack_require__(1);
 
+var _serializeJavascript = __webpack_require__(19);
+
+var _serializeJavascript2 = _interopRequireDefault(_serializeJavascript);
+
 var _Routes = __webpack_require__(2);
 
 var _Routes2 = _interopRequireDefault(_Routes);
@@ -416,7 +420,7 @@ exports.default = function (req, store) {
       )
     )
   ));
-  return '\n    <html>\n      <head>\n      </head>\n      <body>\n        <div id="root">' + content + '</div>\n      </body>\n      <script>\n        window.INITIAL_STATE = ' + JSON.stringify(store.getState()) + '\n      </script>\n      <script src="bundle.js"></script>\n    </html>\n  ';
+  return '\n    <html>\n      <head>\n      </head>\n      <body>\n        <div id="root">' + content + '</div>\n      </body>\n      <script>\n        window.INITIAL_STATE = ' + (0, _serializeJavascript2.default)(store.getState()) + '\n      </script>\n      <script src="bundle.js"></script>\n    </html>\n  ';
 };
 
 /***/ }),
@@ -512,6 +516,12 @@ exports.default = function () {
       return state;
   }
 };
+
+/***/ }),
+/* 19 */
+/***/ (function(module, exports) {
+
+module.exports = require("serialize-javascript");
 
 /***/ })
 /******/ ]);
